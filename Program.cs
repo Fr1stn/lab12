7d4244c18ce96969ab4f084a0afae49dbbf2efcb
Fr1stn <happipampers00@gmail.com>
2025-12-12 18:46:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Напишите что-нибудь(желательно с цифрами):");
+string Mix = Console.ReadLine();
+int x = 0;
+foreach  (char c in Mix)
+{
+    if (c >= '0' && c <= '9')
+    {
+        x++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"{x} цифр в строке.");
